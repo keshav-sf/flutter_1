dd580ab66496e4070c2c6673e53d689b39f1d1ae
Keshav Gupta <keshav.gupta@seaflux.tech>
2024-02-13 11:57:15 +0530

    feat: Add Boilerplate

---
+++ lib/src/models/res_base_model.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'res_base_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ResBaseModel _$ResBaseModelFromJson(Map<String, dynamic> json) => ResBaseModel(
+      error: json['error'] as String?,
+      message: json['message'] as String?,
+    );
+
+Map<String, dynamic> _$ResBaseModelToJson(ResBaseModel instance) =>
+    <String, dynamic>{
+      'error': instance.error,
+      'message': instance.message,
+    };
